96ef7c582099a8a5a177120ce0a8a1e837dd2ef8
Daniel Mera <daniel.mera@epn.edu.ec>
2024-12-01 13:40:23 -0500

    Hasta carrito objeto

---
+++ 01-Unity/Autos/Assets/Scenes/Colision.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("GOLPEEEE");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entrando a trigger");
+        if (other.tag == "Paquete")
+        {
+            Debug.Log("Paquete recogido");
+        }
+        if (other.tag == "Cliente")
+        {
+            Debug.Log("Paquete entregado");
+        }
+    }
+}

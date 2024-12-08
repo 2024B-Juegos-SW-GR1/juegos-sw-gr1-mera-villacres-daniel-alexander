90f533ea8976606bafe892e8aabe35b7127929be
Daniel Mera <daniel.mera@epn.edu.ec>
2024-12-08 13:54:07 -0500

    Add: Recoleccion paquetes y sprite renderer

--- 01-Unity/Autos/Assets/Scenes/Colision.cs
+++ 01-Unity/Autos/Assets/Scenes/Colision.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 
 public class Colision : MonoBehaviour
 {
+    private bool hasPackage;
+    [SerializeField] private float destroyDelay = 0.5f;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("GOLPEEEE");
@@ -13,13 +22,18 @@ public class Colision : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Entrando a trigger");
-        if (other.tag == "Paquete")
+        if (other.tag == "Paquete" && hasPackage == false)
         {
             Debug.Log("Paquete recogido");
+            hasPackage = true;
+            spriteRenderer.color = Color.red;
+            Destroy(other.gameObject, destroyDelay);
         }
-        if (other.tag == "Cliente")
+        if (other.tag == "Cliente" && hasPackage == true)
         {
             Debug.Log("Paquete entregado");
+            hasPackage = false;
+            spriteRenderer.color = Color.green;
         }
     }
 }
